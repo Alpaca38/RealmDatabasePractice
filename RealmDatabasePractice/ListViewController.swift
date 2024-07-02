06ea0b84025b4f14b82fa9e3dd83d76a3ac020d6
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-07-02 16:30:06 +0900

    Initial Commit

---
+++ RealmDatabasePractice/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmDatabasePractice
+//
+//  Created by 조규연 on 7/2/24.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
